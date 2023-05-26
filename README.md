8958944ddbda12f84a26a4ab6b44f542ac83a3c1
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2023-05-26 20:32:48 +0100

    Initial README and scripts setup
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # webui
+
+This is the home of the web user-interface for Galasa.
+
+## Technologies used
+- [Typescript](https://www.typescriptlang.org/)
+- [React](https://react.dev/)
+- [Carbon controls](https://carbondesignsystem.com/all-about-carbon/what-is-carbon/)
+
+
+## Set up
+- Clone the repository
+- Run the `setup-locally.sh` script
+
+## To build locally
+- Run the `build-locally.sh` script
+
+## How to contribute
+See the [contributions.md](./CONTRIBUTIONS.md) file for terms and instructions.
---
+++ build-locally.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+
+
+# Where is this script executing from ?
+BASEDIR=$(dirname "$0");pushd $BASEDIR 2>&1 >> /dev/null ;BASEDIR=$(pwd);popd 2>&1 >> /dev/null
+# echo "Running from directory ${BASEDIR}"
+export ORIGINAL_DIR=$(pwd)
+cd "${BASEDIR}"
+
+
+#--------------------------------------------------------------------------
+# Set Colors
+#--------------------------------------------------------------------------
+bold=$(tput bold)
+underline=$(tput sgr 0 1)
+reset=$(tput sgr0)
+
+red=$(tput setaf 1)
+green=$(tput setaf 76)
+white=$(tput setaf 7)
+tan=$(tput setaf 202)
+blue=$(tput setaf 25)
+
+#--------------------------------------------------------------------------
+#
+# Headers and Logging
+#
+#--------------------------------------------------------------------------
+underline() { printf "${underline}${bold}%s${reset}\n" "$@"
+}
+h1() { printf "\n${underline}${bold}${blue}%s${reset}\n" "$@"
+}
+h2() { printf "\n${underline}${bold}${white}%s${reset}\n" "$@"
+}
+debug() { printf "${white}%s${reset}\n" "$@"
+}
+info() { printf "${white}➜ %s${reset}\n" "$@"
+}
+success() { printf "${green}✔ %s${reset}\n" "$@"
+}
+error() { printf "${red}✖ %s${reset}\n" "$@"
+}
+warn() { printf "${tan}➜ %s${reset}\n" "$@"
+}
+bold() { printf "${bold}%s${reset}\n" "$@"
+}
+note() { printf "\n${underline}${bold}${blue}Note:${reset} ${blue}%s${reset}\n" "$@"
+}
+
+#-----------------------------------------------------------------------------------------                   
+# Functions
+#-----------------------------------------------------------------------------------------                   
+function usage {
+    info "Syntax: build-locally.sh [OPTIONS]"
+    cat << EOF
+Options are:
+-c | --clean : Do a clean build. One of the --clean or --delta flags are mandatory.
+-d | --delta : Do a delta build. One of the --clean or --delta flags are mandatory.
+
+Environment variables used:
+None
+EOF
+}
+
+#--------------------------------------------------------------------------
+# 
+# Main script logic
+#
+#--------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------------------                   
+# Process parameters
+#-----------------------------------------------------------------------------------------
+function process_parameters {
+    export build_type=""
+
+    while [ "$1" != "" ]; do
+        case $1 in
+            -c | --clean )          build_type="clean"
+                                    ;;
+            -d | --delta )          build_type="delta"
+                                    ;;
+            -h | --help )           usage
+                                    exit
+                                    ;;
+            * )                     error "Unexpected argument $1"
+                                    usage
+                                    exit 1
+        esac
+        shift
+    done
+
+    if [[ "${build_type}" == "" ]]; then
+        error "Need to use either the --clean or --delta parameter."
+        usage
+        exit 1  
+    fi
+}
+
+function clean {
+    success "Cleaned up OK"
+}
+
+process_parameters $*
+
+# Clean up if we need to.
+if [[ "${build_type}" == "clean" ]]; then
+    clean
+fi
+
+success "Project built OK."
