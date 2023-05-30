ec247159a354686c9b02aba3d0d0691e8635dd16
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2023-05-30 20:47:19 +0100

    Initial commit. Something which displays, plus some build scripts.
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

---
+++ galasa-ui/src/App.tsx
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <span>Galasa Ecosystem (Experimental)</span>
+    );
+  }
+}
+
+export default App;
