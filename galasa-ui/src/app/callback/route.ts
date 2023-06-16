7b8af4f53e6b2c427191e789249c4ec75472dce0
Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
2023-06-16 15:22:09 +0100

    Add re-auth on token expiry
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>

--- galasa-ui/src/app/callback/route.ts
+++ galasa-ui/src/app/callback/route.ts
@@ -1,21 +1,21 @@
 /*
  * Copyright contributors to the Galasa project
  */
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
+import { openIdClient } from '../auth/route';
 import { cookies } from 'next/headers';
-import { openIdClient } from "../auth/route";
 
 // GET request handler for requests to /callback
 export async function GET(request: Request) {
   const callbackParams = openIdClient.callbackParams(request.url);
   callbackParams.state = undefined;
 
-  const authToken = await openIdClient.callback("http://localhost:3000/callback", callbackParams);
+  const authToken = await openIdClient.callback('http://localhost:3000/callback', callbackParams);
 
   // Set the ID token cookie
   if (authToken.id_token) {
-    cookies().set("id_token", authToken.id_token, { secure: true });
+    cookies().set('id_token', authToken.id_token, { secure: true });
   }
 
-  return NextResponse.redirect(new URL("/", request.url));
-};
+  return NextResponse.redirect(new URL('/', request.url));
+}
