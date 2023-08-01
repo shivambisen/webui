658ab186e6e3a8854c485f454cfaee0b03aca661
Fiona Ampofo <64271621+Akyiaa@users.noreply.github.com>
2023-08-01 11:17:34 +0100

    Update authToken.test.ts

--- galasa-ui/src/tests/routes/authToken.test.ts
+++ galasa-ui/src/tests/routes/authToken.test.ts
@@ -5,8 +5,8 @@
  */
 
 /* 
-* @jest-environment node
-*/
+ * @jest-environment node
+ */
 import * as AuthTokenRoute from '@/app/auth/token/route';
 import * as Auth from '@/utils/auth';
 import * as GrpcClient from '@/utils/grpc/client';
