6d7bc2af0b3873445be2f8d5b164ce91a8f9f9bf
Aashir Siddiqui <aashir_sidiki@hotmail.com>
2024-09-27 11:18:57 +0100

    Home page supports dynamic title from CPS
    Signed-off-by: Aashir Siddiqui <aashir_sidiki@hotmail.com>

--- galasa-ui/src/components/HomeContent.tsx
+++ galasa-ui/src/components/HomeContent.tsx
@@ -9,18 +9,59 @@ import homeGraphic from "../../public/static/homeGraphic.png"
 import { Heading, Section } from "@carbon/react"
 import styles from "../styles/HomeContent.module.css"
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import MarkdownIt from 'markdown-it';
 
 export default function HomeContent() {
+
+    const [markdownContent, setMarkdownContent]: any = useState()
+
+    let md = new MarkdownIt();
+
+    const fetchHomeTitleFromCps = async () => {
+
+        try{
+            const response = await fetch("/home", { method: "GET" });
+
+            if (response.ok) {
+    
+                let markdownFileContent = await response.text();
+    
+                if(markdownFileContent.length >= 64){
+                    markdownFileContent = markdownFileContent.substring(0, 64) + "..."
+                }
+    
+                let result = md.render(markdownFileContent)
+
+                setMarkdownContent(result)
+            }
+        }catch(err){
+            throw new Error("Could not parse markdown into HTML")
+        }
+    }
+
+    useEffect(() => {
+        fetchHomeTitleFromCps()
+    }, [])
 
     return (
         <Section level={1}>
             <div className={styles.homeContentWrapper}>
-                <Heading>Welcome to your Galasa Service</Heading>
-                <Section level={2}>
-                    <h4>Get the most from your Galasa experience by reading the 
-                        <Link className={styles.link} href="https://galasa.dev/" target="_blank" rel="noopener noreferrer"> Galasa documentation.</Link>
-                    </h4>
-                </Section>
+                {
+                    markdownContent ? (
+                        <div dangerouslySetInnerHTML={{ __html: markdownContent }} />
+                    ) : (
+                        <Section>
+                            <Heading>Welcome to your Galasa Service</Heading>
+                            <Section level={2}>
+                                <h4>
+                                    Get the most from your Galasa experience by reading the
+                                    <Link className={styles.link} href="https://galasa.dev/" target="_blank" rel="noopener noreferrer"> Galasa documentation.</Link>
+                                </h4>
+                            </Section>
+                        </Section>
+                    )
+                }
                 <Image className={styles.heroImage} src={homeGraphic} width={680} height={680} alt='home-graphic'></Image>
             </div>
         </Section>
