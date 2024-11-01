9c82ba8269fc8a4f8b815462e14b47265c0a4751
Aashir Siddiqui <aashir_sidiki@hotmail.com>
2024-11-01 14:23:37 +0000

    Deleted unwanted custom interface
    Signed-off-by: Aashir Siddiqui <aashir_sidiki@hotmail.com>

--- galasa-ui/src/utils/interfaces/FrontEndClient.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright contributors to the Galasa project
- *
- * SPDX-License-Identifier: EPL-2.0
- */
-export interface FrontEndClient {
-    clientName: string;
-    lastLogin: string; // Use `Date` type if you want to work directly with Date objects
-}
