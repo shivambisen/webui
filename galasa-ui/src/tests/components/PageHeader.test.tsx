a0933ee99e3d70c5c16bad02ffb79d9282b47441
ShivamBisen <shivambisen16@kgpian.iitkgp.ac.in>
2025-07-12 02:23:26 +0530

    removed unwanted mocked matchmedia functions
    Signed-off-by: ShivamBisen <shivambisen16@kgpian.iitkgp.ac.in>

--- galasa-ui/src/tests/components/PageHeader.test.tsx
+++ galasa-ui/src/tests/components/PageHeader.test.tsx
@@ -42,16 +42,7 @@ jest.mock('next-intl', () => ({
 beforeAll(() => {
   Object.defineProperty(window, 'matchMedia', {
     writable: true,
-    value: jest.fn().mockImplementation(query => ({
-      matches: false, // or true, depending on your test case
-      media: query,
-      onchange: null,
-      addListener: jest.fn(), // deprecated
-      removeListener: jest.fn(), // deprecated
-      addEventListener: jest.fn(), // modern
-      removeEventListener: jest.fn(),
-      dispatchEvent: jest.fn(),
-    })),
+    value: jest.fn().mockImplementation(query => ({})),
   });
 });
 
