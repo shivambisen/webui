f6fd4af96a48749f59ef76562693d5261d47a3b5
ShivamBisen <shivambisen16@kgpian.iitkgp.ac.in>
2025-08-01 06:58:56 +0530

    feat(test-runs): fixed build failure
    Signed-off-by: ShivamBisen <shivambisen16@kgpian.iitkgp.ac.in>

--- galasa-ui/src/tests/components/TestRunsTabs.test.tsx
+++ galasa-ui/src/tests/components/TestRunsTabs.test.tsx
@@ -681,7 +681,7 @@ describe('TestRunsTabs Component', () => {
 
     const defaultTransformedRun = {
       bundle: "N/A", group: "N/A", package: "N/A", result: "N/A", submissionId: "N/A",
-      submittedAt: "-", testName: "N/A", runName: "N/A"
+      submittedAt: "N/A", testName: "N/A", runName: "N/A"
     };
 
     beforeEach(() => {
